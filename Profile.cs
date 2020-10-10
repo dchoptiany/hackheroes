d0ff4f369477e49229b87dabe6a5a18a78665db8
Jakub Choptiany <jakub.choptiany1@gmail.com>
2020-10-10 11:58:02 +0200

    Profile class with user's name, age, weight, height and gender added.

---
+++ Profile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Profile
+{
+	public Profile()
+	{
+		enum Gender
+		{
+			Male,
+			Female
+		}
+		
+		public string name {get; set;}
+	    public uint age {get; set;}
+		public float weight {get; set;}
+		public float height {get; set;}
+		public Gender gender {get; set;}
+
+		Profile(string name, unsigned age, float weight, float height, Gender gender) 
+				:name(name), age(age), weight(weight), height(height), gender(gender) {}
+	}
+}
