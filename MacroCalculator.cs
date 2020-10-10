ab4d1886769a3e9fccaf34504b630542e36e9775
KacperGra <kacperktbm@gmail.com>
2020-10-10 12:55:21 +0200

    Improved scripts
    - Fixed User.cs privacy levels
    - User.cs and MacroCalculator.cs works fine togheter
    - Added test user to Program.cs

--- MacroCalculator.cs
+++ MacroCalculator.cs
@@ -11,28 +11,22 @@ namespace app
         public int carbohydrates;
         public int fat;
 
-        public enum Gender
-        {
-            Male = 0,
-            Female
-        }
-
-        public void CalculateMacro(float weight, float height, int age, Gender gender)
+        public void CalculateMacro(User user)
         {
             float rmr;
-            if (gender == Gender.Male)
+            if (user.gender == Gender.Male)
             {
-                rmr = height * 6.25f + weight * 10f - (age * 5f) + 5f;
+                rmr = user.height * 6.25f + user.weight * 10f - (user.age * 5f) + 5f;
             }
             else
             {
-                rmr = height * 6.25f + weight * 10f - (age * 5f) - 161f;
+                rmr = user.height * 6.25f + user.weight * 10f - (user.age * 5f) - 161f;
             }
 
             calories = (int)(rmr * 1.45f);
             float caloriesLeft = calories;
 
-            protein = (int)weight * 2;
+            protein = (int)user.weight * 2;
             caloriesLeft -= protein * 4;
 
             fat = (int)(caloriesLeft * (0.35f / 9f));
--- Program.cs
+++ Program.cs
@@ -8,9 +8,12 @@ namespace app
 {
     static class Program
     {
+        private static User user = new User("User", 18, 80f, 180f, Gender.Male);
         [STAThread]
         static void Main()
         {
+            
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
