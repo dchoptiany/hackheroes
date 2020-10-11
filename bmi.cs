09cd13167633f77cfd0441c85307395269c054f2
anna2468 <61554513+anna2468@users.noreply.github.com>
2020-10-11 19:06:18 +0200

    Added BMI

---
+++ bmi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app
+{
+    class bmi
+    {
+        public static void CalculateBmi (User user)
+        {
+            
+            float BMI = user.weight / (user.height / 100 * user.height / 100);
+            Console.Write($"Twoje BMI to {BMI}");
+            if (BMI < 18.5) Console.WriteLine("Niedowaga");
+            else if (BMI < 25) Console.WriteLine("Waga prawidlowa");
+            else Console.WriteLine("Nadwaga");
+        }
+    }
+}
