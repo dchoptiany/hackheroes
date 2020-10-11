9fab326461308a4dc907749f268c065245891d22
dchoptiany <damian.choptiany@gmail.com>
2020-10-11 18:55:35 +0200

    Added comments

--- Program.cs
+++ Program.cs
@@ -15,10 +15,10 @@ namespace app
             List<User> users = new List<User>();
             int currentUserIndex = 0;
 
-            var basicUser = new User("User", 18, 80f, 180f, Gender.Male);
+            var basicUser = new User("User", 18, 80f, 180f, Gender.Male); //tworzenie przykladowego uzytkownika
             users.Add(basicUser);
 
-            macroCalculator.CalculateMacro(users[currentUserIndex]);
+            macroCalculator.CalculateMacro(users[currentUserIndex]); //obliczanie makro dla aktulanego uzytkownika
             Console.WriteLine(users[currentUserIndex].age);
 
             Application.EnableVisualStyles();
