8a39c132055e33ccc406c7739ced505927345d5e
basia156 <basiaparzonka3@wp.pl>
2020-10-11 19:54:57 +0200

    Refaktor

--- bmi.cs
+++ bmi.cs
@@ -6,16 +6,11 @@ using System.Threading.Tasks;
 
 namespace app
 {
-    class BMI
+    static class BMI
     {
-        public static void CalculateBmi (User user)
+        public static void CalculateBmi (ref User user)
         {
-            
-            float BMI = user.weight / (user.height / 100 * user.height / 100);
-            Console.Write($"Twoje BMI to {BMI}");
-            if (BMI < 18.5) Console.WriteLine("Niedowaga");
-            else if (BMI < 25) Console.WriteLine("Waga prawidlowa");
-            else Console.WriteLine("Nadwaga");
+            user.BMI = user.weight / (user.height / 100 * user.height / 100);
         }
     }
 }
