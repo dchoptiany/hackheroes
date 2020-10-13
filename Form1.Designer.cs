c48938dbb6bb50d5c2bd1cd10992e232fdfaf7b6
KacperGra <kacperktbm@gmail.com>
2020-10-13 20:10:42 +0200

    Added Survey.cs class

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -51,8 +51,6 @@
             this.panel5 = new System.Windows.Forms.Panel();
             this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.panel6 = new System.Windows.Forms.Panel();
             this.buttonEdit = new System.Windows.Forms.Button();
@@ -359,8 +357,6 @@
             // flowLayoutPanel2
             // 
             this.flowLayoutPanel2.Controls.Add(this.button1);
-            this.flowLayoutPanel2.Controls.Add(this.button2);
-            this.flowLayoutPanel2.Controls.Add(this.button3);
             this.flowLayoutPanel2.Location = new System.Drawing.Point(75, 101);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
             this.flowLayoutPanel2.Size = new System.Drawing.Size(800, 375);
@@ -376,24 +372,6 @@
             this.button1.Text = "Poziom aktywności fizycznej";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(3, 59);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(794, 50);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(3, 115);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(794, 50);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
             // 
             // label2
             // 
@@ -1091,8 +1069,6 @@
         private System.Windows.Forms.PictureBox pictureBoxArrow;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
     }
 }
 
---
+++ Survey.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app
+{
+    class Survey
+    {
+        public enum QuestionType
+        {
+            YES_OR_NO,
+            INPUT_VALUE
+        }
+
+        struct Question
+        {
+            public Question(string _questionTitle, QuestionType _questionType)
+            {
+                questionTitle = _questionTitle;
+                questionType = _questionType;
+            }
+
+            string questionTitle;
+            QuestionType questionType;
+        }
+
+        private List<Question> questions;
+
+        public Survey()
+        {
+            questions = new List<Question>();
+        }
+
+        public void AddQuestion(string title, QuestionType type)
+        {
+            questions.Add(new Question(title, type));
+        }
+    }
+}
