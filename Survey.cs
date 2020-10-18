87627ac9d1f046a8ce47a3d0b31ed96f6458ce4e
KacperGra <kacperktbm@gmail.com>
2020-10-18 10:12:15 +0200

    Update Survey.cs

--- Survey.cs
+++ Survey.cs
@@ -12,41 +12,43 @@ namespace app
         public string title;
         public List<Question> questions;
 
-        public enum QuestionType
-        {
-            YES_OR_NO,
-            INPUT_VALUE,
-            ABCD
-        }
-
-        public struct Question
-        {
-            public string questionTitle;
-            public QuestionType questionType;
-
-            public Question(string _questionTitle, QuestionType _questionType)
-            {
-                questionTitle = _questionTitle;
-                questionType = _questionType;
-            }
-
-            public struct Answers
-            {
-                public List<KeyValuePair<string, uint>> answersValues;
-
-                public Answers(List<KeyValuePair<string, uint>> _answersValues)
-                {
-                    answersValues = _answersValues;
-                }
-            }
-
-        }      
-
         public Survey(string _title)
         {
             title = _title;
             questions = new List<Question>();
         }
+
+        public enum QuestionType
+        {
+            YES_OR_NO,
+            ABCD
+        }
+
+        public class Question
+        {
+            public string questionTitle;
+            public QuestionType questionType;
+            public List<KeyValuePair<string, uint>> answersValues;
+
+            public Question(string _questionTitle, QuestionType _questionType)
+            {     
+                questionTitle = _questionTitle;
+                questionType = _questionType;
+
+                answersValues = new List<KeyValuePair<string, uint>>();
+                if (questionType == QuestionType.YES_OR_NO)
+                {
+                    AddAnswer("No", 0);
+                    AddAnswer("Yes", 1);
+                }
+            }
+
+            public void AddAnswer(string answerText, uint answerValue)
+            {
+                KeyValuePair<string, uint> newPair = new KeyValuePair<string, uint>(answerText, answerValue);
+                answersValues.Add(newPair);
+            }
+        }      
 
         public void AddQuestion(string title, QuestionType type)
         {
