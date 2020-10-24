15b872bfc52fb40e454866d746fa4e7370965299
dchoptiany <damian.choptiany@gmail.com>
2020-10-24 21:17:30 +0200

    Revert "Added survey about sleep"
    This reverts commit 4617ac7c17e31c7ca399a761c2bef73981639de1.

--- Form1.cs
+++ Form1.cs
@@ -948,11 +948,10 @@ namespace app
                         textBoxSurveyText.Visible = false;
                         buttonSurveyConfirm.Visible = false;
 
-                        buttonSurveyNo.Text = surveys[currentSurveyIndex].questions[Survey.currentQuestionIndex].answersValues[0].Key;
-                        buttonSurveyYes.Text = surveys[currentSurveyIndex].questions[Survey.currentQuestionIndex].answersValues[1].Key;
-                        buttonSurveyA.Text = surveys[currentSurveyIndex].questions[Survey.currentQuestionIndex].answersValues[2].Key;
-                        buttonSurveyB.Text = surveys[currentSurveyIndex].questions[Survey.currentQuestionIndex].answersValues[3].Key;
-                        
+                        buttonSurveyA.Text = surveys[currentSurveyIndex].questions[Survey.currentQuestionIndex].answersValues[0].Key;
+                        buttonSurveyB.Text = surveys[currentSurveyIndex].questions[Survey.currentQuestionIndex].answersValues[1].Key;
+                        buttonSurveyYes.Text = surveys[currentSurveyIndex].questions[Survey.currentQuestionIndex].answersValues[2].Key;
+                        buttonSurveyNo.Text = surveys[currentSurveyIndex].questions[Survey.currentQuestionIndex].answersValues[3].Key;
                         break;
                     } 
                 case QuestionType.INPUT:
@@ -1044,7 +1043,7 @@ namespace app
 
                 Calculator.CalculateActivityLevel(users[currentUserIndex]);
                 labelFinish.Text = "Poziom aktywności użytkownika został zaktualizowany.";
-            }
+            }       
             
             Center(labelFinish);
         }
